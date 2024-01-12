ede491c1f996a1cf2fc06fadb6f6e879acf57f8b
samellow <sammykaris48@gmail.com>
2024-01-12 12:23:31 +0300

    first commit

---
+++ backend/config.js
@@ -0,0 +1,2 @@
+export const PORT = 3000;
+export const mongoDBURL = 'mongodb+srv://sam:sammy13612@cluster0.gskd4oa.mongodb.net/bookStore?retryWrites=true&w=majority';
---
+++ backend/index.js
@@ -0,0 +1,123 @@
+import  express  from "express";
+import { PORT, mongoDBURL} from "./config.js";
+import mongoose from "mongoose";
+import Book from './models/bookModel.js';
+
+const app = express();
+
+//Middleware for parsing request body 
+app.use(express.json());
+
+
+app.get('/', (req,res)=>{
+   console.log(req);
+   return res.status(234).send("Welcome to my MERN app");
+});
+
+//Route for saving a new book
+app.post('/books',async(req,res)=>{
+    try{
+        if (
+            !req.body.title ||
+            !req.body.author ||
+            !req.body.publishYear
+        ) {
+            return res.status(400).send({
+                message: 'Send all required Fields: title, author, publish year'
+            });
+        }
+
+        const newBook = {
+            title: req.body.title,
+            author: req.body.author,
+            publishYear: req.body.publishYear,
+        };
+
+        const book = await Book.create(newBook);
+
+        return res.status(201).send(book);
+
+    } catch(error){
+        console.log(error.message)
+        res.status(500).send({message: error.message});
+    }
+
+});
+
+// Route for Get All books from database 
+app.get('/books',async(req,res)=>{
+    try{
+        const books = await Book.find({});
+
+        return res.status(200).json({
+            count: books.length,
+            data: books
+        });
+    } catch (error){
+        console.log(error.message);
+        res.status(500).send({message: error.message});
+    }
+
+});
+
+// Route for getting one book from database
+app.get('/books/:id',async(req,res)=>{
+    try{
+        const { id } = req.params;
+
+        const book = await Book.findById(id);
+
+
+        return res.status(200).json(book);
+    } catch (error){
+        console.log(error.message);
+        res.status(500).send({message: error.message});
+    }
+
+});
+
+// Route for updating a book
+
+app.put('/books/:id', async(req,res)=>{
+    try {
+        if (
+            !req.body.title ||
+            !req.body.author ||
+            !req.body.publishYear 
+        ) {
+            return res.status(400).send({
+                message: 'Send All required fields: Title, author, publish year'
+            })
+        }
+            const { id } = req.params;
+
+            const result = await Book.findByIdAndUpdate(id, req.body);
+
+            if(!result) {
+                return res.status(404).send({ message: 'Book not Found'});
+
+            }
+            
+            return res.status(200).send({ message: 'Book updated Successfully!'});
+        
+
+    }catch (error){
+        console,log(error.message);
+        res.status(500).send({ message: error.message});
+    }
+})
+
+//mongoose connection 
+mongoose.connect(mongoDBURL)
+.then(()=>{
+    console.log('App connected to database');
+    app.listen(PORT,()=>{
+        console.log(`App is listening on port: ${PORT}`)
+    });
+})
+.catch((error)=>{
+ console.log(error);
+})
+
+
+
